70e3f26d13e7e0c7cb3f6bbc99742985dc8e7285
songkey <songkey@outlook.com>
2016-02-25 20:01:24 +0800

    add redisDB.py

---
+++ redisDB.py
@@ -0,0 +1,21 @@
+import redis
+
+class redisDB:
+  def __init__(self):
+    self.host = "localhost"
+    self.port = 6379
+
+  def write(self, key, val):
+    try:
+      r = redis.StrictRedis(host=self.host, port=self.port)
+      r.set(key, val) 
+    except Exception, exception:
+      print exception
+  def read(self, key):
+    try:
+      r = redis.StrictRedis(host=self.host, port=self.port)
+      val = r.get(key)
+      print val
+      return val
+    except Exception, exception:
+      print exception
