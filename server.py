1059c4d32726d6896ed3d20a87e0598db9d981c1
songkey <songkey@outlook.com>
2016-02-25 21:00:04 +0800

    add getState setState function for server

--- server.py
+++ server.py
@@ -3,4 +3,16 @@ import json
 class GameServer:
   def __init__(self):
     db = redisDB("120.27.29.9")
-  def setState:
+    self.setState("none", 0)
+
+  # Stage : none, start, end
+  # Round : 1, 2, ...
+  def setState(self, Stage, Round):
+    val = {"Stage" : Stage, "Round" : str(Round)}
+    self.db.write("State", json.dumps(val))
+
+  def getState(self):
+    val = self.db.read("State")
+    objs = json.loads(val)
+    return objs["Stage"], objs["Round"]
+    
