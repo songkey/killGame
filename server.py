fc67bfdc6db6eb329e4cb5861bd21f270adeb69c
songkey <songkey@outlook.com>
2016-02-25 23:03:24 +0800

    add serval functions for server

--- server.py
+++ server.py
@@ -1,18 +1,37 @@
 import redisDB
 import json
 class GameServer:
-  def __init__(self):
+  def __init__(self, MemberNum):
     db = redisDB("120.27.29.9")
-    self.setState("none", 0)
+    self.MemberNum = MemberNum
+    self.setState("none", 0, MemberNum)
 
   # Stage : none, start, end
   # Round : 1, 2, ...
-  def setState(self, Stage, Round):
-    val = {"Stage" : Stage, "Round" : str(Round)}
+  # MemberNum : i > 0
+  def setState(self, Stage, Round, MemberNum):
+    val = {"global_Stage" : Stage, "Round" : str(Round), \
+           "MemberNum" : str(MemberNum)}
     self.db.write("State", json.dumps(val))
 
   def getState(self):
-    val = self.db.read("State")
+    val = self.db.read("global_State")
     objs = json.loads(val)
-    return objs["Stage"], objs["Round"]
+    return objs["Stage"], int(objs["Round"]), int(objs["MemberNum"])
     
+  def getMembers(self):
+    val = self.db.read("global_Members")
+    objs = json.loads(val)
+    if len(objs) <= self.MemberNum : return objs
+    else return objs[0:self.MemberNum]
+
+  def getQuestion(self, player):
+    key = "global_" + player + "_question"
+    val = self.db.read(key)
+    return val
+
+  # player : player name
+  # answer : true or false
+  def setAnswer(self, player, answer):
+    key = "global_" + player + "_question"
+    self.db.write(key, answer): 
