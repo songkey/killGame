53b02cc6ffa306dd5632fb8c4d7fcda3c267ef81
songkey <songkey@outlook.com>
2016-03-13 17:31:39 +0800

    add mget mset

--- redisDB.py
+++ redisDB.py
@@ -4,29 +4,37 @@ class redisDB:
   def __init__(self, hostName):
     self.host = hostName
     self.port = 6379
-    r = None
+
+  def kset(self, key, val):
     try:
       r = redis.StrictRedis(host=self.host, port=self.port)
-    except Exception, exception:
-      print exception
-    
-
-  def write(self, key, val):
-    try:
-      if r == None : r = redis.StrictRedis(host=self.host, port=self.port)
       r.set(key, val) 
     except Exception, exception:
       print exception
 
-  def read(self, key):
+  def kget(self, key):
     try:
-      if r == None : r = redis.StrictRedis(host=self.host, port=self.port)
+      r = redis.StrictRedis(host=self.host, port=self.port)
       val = r.get(key)
       print val
       return val
     except Exception, exception:
       print exception
 
+  def kmset(KeyValDict):
+    try:
+      r = redis.StrictRedis(host=self.host, port=self.port)
+      return r.mset(KeyValDict)
+    except Exception, exception:
+      print exception
+      
+  def kmget(KeyList):
+    try:
+      r = redis.StrictRedis(host=self.host, port=self.port)
+      return r.mget(KeyList)
+    except Exception, exception:
+      print exception
+    
   def listAdd(self, key, val):
     try:
       if r == None : r = redis.StrictRedis(host=self.host, port=self.port)
